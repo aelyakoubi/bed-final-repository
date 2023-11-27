f97e287d1c30b3237ef29986255c86c0b2f43dbf
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-11-27 11:00:18 +0100

    change prisma and seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,65 +1,106 @@
 import { PrismaClient } from "@prisma/client";
-import userData from "../src/data/users.json" assert { type: "json" };
-import amenityData from "../src/data/amenities.json" assert { type: "json" };
-import reviewData from "../src/data/reviews.json" assert { type: "json" };
-import propertyData from "../src/data/properties.json" assert { type: "json" };
-import bookingData from "../src/data/bookings.json" assert { type: "json" };
-import hostData from "../src/data/hosts.json" assert { type: "json" };
+import amenitiesData from "../src/data/amenities.json" assert {type: "json"}
+import bookingsData from "../src/data/bookings.json" assert {type: "json"}
+import hostsData from "../src/data/hosts.json" assert {type: "json"}
+import propertiesData from "../src/data/properties.json" assert {type: "json"}
+import reviewsData from "../src/data/reviews.json" assert {type: "json"}
+import usersData from "../src/data/users.json" assert {type: "json"}
 
-const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
+const prisma = new PrismaClient({log: ["query", "info", "warn", "error"] });
 
-async function seed() {
-  try {
-    for (const amenity of amenityData.amenities) {
-      await prisma.amenity.upsert({
-        where: { id: amenity.id },
-        update: {},
-        create: amenity,
-      });
+async function main() {
+    const { amenities } = amenitiesData;
+    const { bookings } = bookingsData
+    const { hosts } = hostsData
+    const { properties } = propertiesData
+    const { reviews } = reviewsData
+    const { users } = usersData
+
+    for (const user of users) {
+        await prisma.user.upsert({
+            where: { id: user.id },
+            update: {},
+            create: user
+        })
     }
-    for (const booking of bookingData.bookings) {
-      await prisma.booking.upsert({
-        where: { id: booking.id },
-        update: {},
-        create: booking,
-      });
+
+    for (const booking of bookings) {
+        await prisma.booking.upsert({
+            where: { id: booking.id },
+            update: {},
+            create: {
+                ...booking,
+                userId: {
+                    connect: { id: booking.user }
+                },
+                propertyId: {
+                    connect: { id: booking.property }
+                }
+            },
+
+        })
     }
-    for (const host of hostData.hosts) {
-      await prisma.host.upsert({
-        where: { id: host.id },
-        update: {},
-        create: host,
-      });
+
+    for (const property of properties) {
+        await prisma.property.upsert({
+            where: { id: property.id },
+            update: {},
+            create: {
+                ...property,
+                amenities: {
+                    connect: property.amenities.map((id) => ({ id }))
+                },
+                reviews: {
+                    connect: property.reviews.map((id) => ({ id }))
+                },
+                bookings: {
+                    connect: property.bookings.map((id) => ({ id }))
+                }
+            }
+        })
     }
-    for (const property of propertyData.properties) {
-      await prisma.property.upsert({
-        where: { id: property.id },
-        update: {},
-        create: property,
-      });
+
+    for (const amenity of amenities) {
+        await prisma.amenity.upsert({
+            where: { id: amenity.id },
+            update: {},
+            create: amenity,
+        });
     }
-    for (const review of reviewData.reviews) {
-      await prisma.review.upsert({
-        where: { id: review.id },
-        update: {},
-        create: review,
-      });
+
+
+
+    for (const review of reviews) {
+        await prisma.review.upsert({
+            where: { id: review.id },
+            update: {},
+            create: {
+                ...review,
+                userId: {
+                    connect: { id: review.user }
+                },
+                propertyId: {
+                    connect: { id: review.property }
+                }
+            }
+        })
     }
-    for (const user of userData.users) {
-      await prisma.user.upsert({
-        where: { id: user.id },
-        update: {},
-        create: user,
-      });
+
+    for (const host of hosts) {
+        await prisma.host.upsert({
+            where: { id: host.id },
+            update: {},
+            create: host
+        })
     }
-    console.log("Seed data inserted successfully.");
-  } catch (error) {
-    console.error("Error seeding the database:", error);
-  } finally {
-    await prisma.$disconnect();
-  }
 }
 
-seed().catch((e) => {
-  throw e;
-});
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
