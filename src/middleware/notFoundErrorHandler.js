b20a4da82db3f0412735473693f0b2a09c04f152
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-12-14 12:14:40 +0100

    x

--- src/middleware/notFoundErrorHandler.js
+++
@@ -1,8 +0,0 @@
-const notFoundErrorHandler = (err, req, res, next) => {
-  if (err.name === "NotFoundError") {
-    return res.status(404).json({ message: err.message });
-  }
-  next(err);
-};
-
-export default notFoundErrorHandler;
