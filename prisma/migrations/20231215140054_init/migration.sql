51900f8f44c61d9bb134b85d66f3cd3e345a10c1
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-12-15 15:03:37 +0100

    modif ENDPOINTS REQUIREMENTS PROJECT ARE INCORRECT

--- prisma/migrations/20231215140054_init/migration.sql
+++ prisma/migrations/20231215140054_init/migration.sql
@@ -1,60 +1,14 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `_id` VARCHAR(191) NOT NULL,
+    `id` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `phoneNumber` VARCHAR(191) NULL,
-    `ProfilePicture` VARCHAR(191) NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `profilePicture` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `User_username_key`(`username`),
-    PRIMARY KEY (`_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Booking` (
-    `id` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `propertyId` VARCHAR(191) NOT NULL,
-    `checkinDate` DATETIME(3) NOT NULL,
-    `checkoutDate` DATETIME(3) NOT NULL,
-    `numberOfGuests` INTEGER NOT NULL,
-    `totalPrice` INTEGER NOT NULL,
-    `bookingsStatus` VARCHAR(191) NOT NULL,
-
-    INDEX `Booking_userId_idx`(`userId`),
-    INDEX `Booking_propertyId_idx`(`propertyId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Review` (
-    `id` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `propertyId` VARCHAR(191) NOT NULL,
-    `rating` INTEGER NOT NULL,
-    `comment` VARCHAR(191) NOT NULL,
-
-    INDEX `Review_userId_idx`(`userId`),
-    INDEX `Review_propertyId_idx`(`propertyId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Property` (
-    `id` VARCHAR(191) NOT NULL,
-    `hostId` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `location` VARCHAR(191) NOT NULL,
-    `pricePerNight` DECIMAL(10, 2) NOT NULL,
-    `bedroomCount` INTEGER NOT NULL,
-    `bathroomCount` INTEGER NOT NULL,
-    `maxGuestCount` INTEGER NOT NULL,
-    `rating` INTEGER NOT NULL,
-
-    INDEX `Property_hostId_idx`(`hostId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -74,10 +28,56 @@ CREATE TABLE `Host` (
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `phoneNumber` VARCHAR(191) NOT NULL,
-    `ProfilePicture` VARCHAR(191) NOT NULL,
+    `profilePicture` VARCHAR(191) NOT NULL,
     `aboutMe` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `Host_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Property` (
+    `id` VARCHAR(191) NOT NULL,
+    `hostId` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `pricePerNight` DECIMAL(10, 2) NOT NULL,
+    `bedroomCount` INTEGER NOT NULL,
+    `bathRoomCount` INTEGER NOT NULL,
+    `maxGuestCount` INTEGER NOT NULL,
+    `rating` INTEGER NOT NULL,
+
+    INDEX `Property_hostId_idx`(`hostId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Review` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `propertyId` VARCHAR(191) NOT NULL,
+    `rating` INTEGER NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+
+    INDEX `Review_userId_idx`(`userId`),
+    INDEX `Review_propertyId_idx`(`propertyId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Booking` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `propertyId` VARCHAR(191) NOT NULL,
+    `checkinDate` DATETIME(3) NOT NULL,
+    `checkoutDate` DATETIME(3) NOT NULL,
+    `numberOfGuests` INTEGER NOT NULL,
+    `totalPrice` INTEGER NOT NULL,
+    `bookingStatus` VARCHAR(191) NOT NULL,
+
+    INDEX `Booking_userId_idx`(`userId`),
+    INDEX `Booking_propertyId_idx`(`propertyId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
